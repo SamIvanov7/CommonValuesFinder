27ee818642a42ae5d3e9fe4ad1f8e50e232d81fa
Sam Ivanov <si@juno-solar.com>
2024-02-04 12:16:13 +0100

    Add project files.

---
+++ CommonValues.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SolarPowerEngineering
+{
+    public static class CommonValues
+    {
+        public static IEnumerable<int> FindCommonValues(int[] array1, int[] array2)
+        {
+            return array1.Intersect(array2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SolarPowerEngineering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 2, 3, 4, 5 };
+            int[] array2 = { 3, 4, 5, 6, 7 };
+
+            var commonValues = CommonValues.FindCommonValues(array1, array2);
+
+            Console.WriteLine("Common values in both arrays:");
+            foreach (var value in commonValues)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
